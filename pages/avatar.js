2319e84bb0f008809b5267bc3752139101a0fc1c
hebert villafuerte <hebert.villafuerte.c@hotmail.com>
2021-09-06 13:21:11 -0500

    codigo base

---
+++ pages/avatar.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+
+export default function Avatar(){
+
+    return(
+        <>
+        <Head>
+
+        <meta
+        property="og:title"
+        content="Betsocial - La red social para apostadores"
+        />
+        <meta
+        name="description"
+        content="Red social para apostadores - genera dinero ofreciendo pronósticos."
+        />
+        <meta property="og:url" content="https://www.betsocial.club" />
+       
+        </Head>
+        <div>
+            hola mis reyes aea marika
+            good 
+            <br /> asdasd
+            <br />
+            tu sorisa
+        </div>
+        </>
+    )
+}
+
+
